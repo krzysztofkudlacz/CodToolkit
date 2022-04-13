bd7c798b95838a6d2a667df549d156bfc0abfd51
Krzysztof Kudłacz <krzysiek.kudlacz@gmail.com>
2022-04-13 20:24:20 +0200

    Query COD server over http

---
+++ CodToolkit/Cod/CodServerCommunication.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Flurl;
+using Flurl.Http;
+
+namespace CodToolkit.Cod
+{
+    public class CodServerCommunication
+    {
+        public const string CodUri = "https://www.crystallography.net/cod/result";
+
+        public async Task<IReadOnlyList<CodEntryInfo>> QueryCod()
+        {
+            var codEntries = await GetEntriesInfo();
+            for (var i = 0; i < codEntries.Count; i++)
+                codEntries[i].NumberInCollection = i + 1;
+
+            return codEntries;
+        }
+
+        private async Task<IReadOnlyList<CodEntryInfo>> GetEntriesInfo()
+        {
+            var requiredElements = new List<string> { "Ca", "C", "O", "Mg" };
+            var excludedElements = new List<string> { "H", "Al", "B", "Si" };
+
+            const string dataFormat = "json";
+
+            var codUrl = new Url(CodUri);
+
+            for (var i = 0; i < requiredElements.Count; i++)
+                codUrl.SetQueryParam($"el{i + 1}", requiredElements[i]);
+
+            for (var i = 0; i < excludedElements.Count; i++)
+                codUrl.SetQueryParam($"nel{i + 1}", excludedElements[i]);
+
+            codUrl.SetQueryParam("format", dataFormat);
+
+            var entries = await codUrl.GetAsync().ReceiveJson<List<CodEntryInfo>>();
+
+            return entries;
+        }
+    }
+}
--- CodToolkit/MainWindow.xaml.cs
+++ CodToolkit/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using CodToolkit.Cod;
 
 namespace CodToolkit
 {
@@ -7,9 +11,44 @@ namespace CodToolkit
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static readonly RoutedCommand QueryCodCommand = new RoutedCommand();
+
+        private readonly CodServerCommunication _codServerCommunication;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _codServerCommunication = new CodServerCommunication();
+        }
+
+        private async void QueryCodExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                Mouse.OverrideCursor = Cursors.Wait;
+            });
+
+            var codEntries = await _codServerCommunication.QueryCod();
+
+            CodEntries.ItemsSource = codEntries.Select(entry => new
+            {
+                Number = entry.NumberInCollection, 
+                Id = entry.FileId,
+                Parameters = entry.CellParameters,
+                entry.Formula,
+                entry.SpaceGroup
+            });
+
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                Mouse.OverrideCursor = null;
+            });
+        }
+
+        private void CanQueryCod(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
         }
     }
 }
